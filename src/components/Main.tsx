b4d8c847019b669117271d9c7b16d446d5251674
Sai Bobby <saivkadiyala97@gmail.com>
2021-05-21 15:25:20 -0400

    intial setup
    added all the required modules and a footer, next testing the terminal

---
+++ src/components/Main.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import Terminal from './Terminal';
+import Footer from './footer/Footer';
+
+
+const Main = () =>{
+    return(
+        <div>
+            <Terminal />
+            <Footer />
+        </div>
+    );
+}
+
+export default Main;
---
+++ src/components/Terminal.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import './styles.css';
+
+const Terminal: React.FC = () =>{
+
+    return(
+        <div className="terminal-container">
+            Terminal
+        </div>
+    )
+}
+
+export default Terminal;
