3136f4b8c61dc4283f5fd8b1c7d85b4a6e342eeb
Sai Bobby <saivkadiyala97@gmail.com>
2021-05-27 18:59:01 -0400

    input field styling

--- src/components/Main.tsx
+++ src/components/Main.tsx
@@ -6,7 +6,7 @@ import Header from './header/Header';
 
 const Main: React.FC = () =>{
     return(
-        <div>
+        <div className="main">
             <Header />
             <Terminal />
             <Footer />
--- src/components/terminal/Terminal.tsx
+++ src/components/terminal/Terminal.tsx
@@ -1,34 +1,71 @@
-import { Container } from '@material-ui/core';
+import { Container, makeStyles } from '@material-ui/core';
+import { InvertColorsOffOutlined } from '@material-ui/icons';
 import React, { useState } from 'react';
 import './styles.css';
 
+
+const useStyles = makeStyles(theme=> ({
+    terminalContainer: {
+        backgroundColor: "#1e1e1e",
+        display: "flex",
+        margin:"auto",
+        width: "80%",
+        border:"1px solid black"
+    
+    },
+    commandInput: {
+        background: "#1e1e1e",
+        outline: "none",
+        border: "none",
+        color: "white",
+        textShadow: "0 0 0 gray",
+        fontSize: "18px",
+        fontFamily: "Consolas",
+        fontWeight: 200,
+        "&:focus": {
+          outline: "0px solid transparent;",
+          border: "none"
+        }
+      }
+}));
+
 const Terminal: React.FC = () =>{
+    const classes = useStyles({});
+    const [searchCmd,setSearchCmd] = useState('/help');
+    const [terminal,setTerminal] = useState({})
+
     const handleInputChange = (e:any)=>{
-        console.log(e.target.value);
-        setSearchCmd(e.target.value);   
-    }
+            console.log(e.target.value);
+            setSearchCmd(e.target.value);
+            e.target.style.width = e.target.value.length+10 + "ch";
+    };
+    
+
     const handleKeyDown = (e:React.KeyboardEvent<HTMLInputElement>) => {
         if(e.key==="Enter"){
             e.preventDefault();
+            setSearchCmd("");
             console.log("Enter key is pressed")
         }
     }
 
-    const [searchCmd,setSearchCmd] = useState('/')
+    
 
     return(
         <>
-        <Container className="terminal-container">
+        <div>
             Terminal
-        </Container>
+        </div>
+        <div className={classes.terminalContainer}>
 
-        <input type="text" 
-        placeholder="input any command" 
-        onChange={handleInputChange} value={searchCmd}
-        onKeyDown = {e=>handleKeyDown(e)}
-        >
+            <input type="text"
+            id="searchCmd"
+            className = {classes.commandInput} 
+            onChange={handleInputChange} value={searchCmd}
+            onKeyDown = {e=>handleKeyDown(e)}></input>
 
-        </input>
+        </div>
+        
         
         </>
     );
